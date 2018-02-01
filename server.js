017337f418338edae44377ed40fb70530378398a
oleg <o.d.galimov@yandex.ru>
2018-02-01 13:45:44 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const expr = require ('express');
+const hbs = require ('hbs');
+let app = expr();
+app.set('view engine','hbs');
+
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamer', (text)=>{
+    return text.toUpperCase();
+});
+
+app.use((req,res,next)=>{
+    //перехват входящих запросов
+    console.log(`Событие: ${new Date().toString()} произошло`);
+    console.log(`${req.method} ${req.url} ${req.cookies}`);
+    next();
+});
+//заглушка на все случаи жизни
+app.use((req,res,next)=>{
+    res.render('maintenace.hbs');
+});
+
+app.use(expr.static(__dirname+'/views'));
+app.get(
+    '/',
+    (req,res)=>{
+        console.log('Get for root dir');
+        console.log(req.headers);
+        // res.send('<h1>Hellow Express!</h1>');
+        res.send({
+            age:27,
+            name:'Oleg'
+        });
+    }
+);
+app.get(
+    '/about',
+    (req, res)=>{
+        res.render('about.hbs', {
+            title: 'MyPage',
+            version: process.env.version
+        });
+    }
+);
+
+app.listen(3000, ()=>{console.log('Server is up on 3000 port')});
